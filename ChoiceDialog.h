bae782be7610d0aae6da825910fd9acd59f31a9b
Chanawee124 <78196908+Chanawee124@users.noreply.github.com>
2021-05-18 18:53:46 +0700

    Update ChoiceDialog.h

--- ChoiceDialog.h
+++ ChoiceDialog.h
@@ -2,14 +2,14 @@
 #define CHOICEDIALOG_H
 #include<string>
 #include"Dialog.h"
-#define SIZE 5 
  
-class ChoiceDialog : private Dialog {
+class ChoiceDialog : public Dialog {
 	string* choices;
 public:
-	ChoiceDialog(const string ctt, const string choi1, const string choi2, const string choi3, const string choi4);
+	ChoiceDialog(const string ctt,string* c, int Size);
 	~ChoiceDialog();
-	friend ostream& operator<< (ostream& s, const ChoiceDialog& x);
+	//friend ostream& operator<< (ostream& s, const ChoiceDialog& x);
+	void display(int Size) const;
 };
 
 #endif

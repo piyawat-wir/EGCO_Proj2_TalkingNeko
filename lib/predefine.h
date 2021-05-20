7143c62a4a7e890c6d8223005a8d38d4bb5637d8
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2021-05-21 00:44:10 +0700

    Add files via upload

---
+++ lib/predefine.h
@@ -0,0 +1,14 @@
+#include "display.h"
+#include "pixelimg.h"
+
+PixelImg img_angry("img/angry.txt");
+PixelImg img_bigsmile("img/bigsmile.txt");
+PixelImg img_cheer("img/cheer.txt");
+PixelImg img_confused("img/confused.txt");
+PixelImg img_cry("img/cry.txt");
+PixelImg img_drunk("img/drunk.txt");
+PixelImg img_normal("img/normal.txt");
+PixelImg img_readbook("img/readbook.txt");
+PixelImg img_sad("img/sad.txt");
+PixelImg img_shy("img/shy.txt");
+PixelImg img_weirdsmile("img/wierdsmile.txt");
